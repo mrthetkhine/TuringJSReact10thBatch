85ff71b16b3b0586f11d4be171a78319b1182e23
thetkhine <macbook@MacBooks-MacBook-Pro.local>
2025-01-04 20:59:55 +0630

    init

---
+++ chapter1/Hello.java
@@ -0,0 +1,9 @@
+public class Hello
+{
+    public static void main(String[]args)
+    {
+        System.out.println("Hello World");
+        int data = 5/2;
+        System.out.println(data);
+    }
+}
---
+++ chapter1/helloworld.js
@@ -0,0 +1,6 @@
+console.log('Hello World');
+let data = 5/2;
+console.log(data);
+data = [1,10,5]
+console.log('Min ',Math.min(...data));
+console.log('Min ',Math.min.apply(null,data));
