0856e3aaf5d21e2d348743a08c361d4e9a5893a9
thetkhine <macbook@MacBooks-MacBook-Pro.local>
2025-06-23 21:55:22 +0630

    add auth

--- next-zustand/src/app/hooks/api/authApi.ts
+++ next-zustand/src/app/hooks/api/authApi.ts
@@ -1,7 +1,7 @@
 
 import axiosInstance from "@/app/axiosInstance";
 import LoginUser, {LoginResponse} from "@/types/auth";
-
+//update
 export async function apiLogin(user:LoginUser):Promise<LoginResponse>{
     const reviewResponse =  await axiosInstance.post<LoginResponse>(`/api/users/login`,user);
     return reviewResponse.data;
